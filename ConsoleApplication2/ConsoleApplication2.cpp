43c7b50538fe0b995b8f337f49b1edd1535181cc
Alienware <Alienware@DESKTOP-MHOC38H>
2019-05-05 07:12:57 -0400

    4 change

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -22,7 +22,7 @@ change 3
 
 // Tips for Getting Started: 
 //   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
+//   2. Use the Team E            xplorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
